b3c33bc029e958334b3a44ffea8786e14197bfd5
Gabriela Ceranto <gabiceranto@gmail.com>
2022-10-29 16:13:52 -0300

    fatorial com recusividade exercício

---
+++ fatorial-recursividade.js
@@ -0,0 +1,19 @@
+/*
+Seja n um número natural, tal que n >= 2, chama-se fatorial de n o produto de todos os números naturais consecutivos de n até 1.
+Implemente um programa (utilizando laços) que receba um número e retorne o fatorial desse número.
+*/
+var prompt = require('prompt-sync')();
+let num = parseInt (prompt('Digite um número para fatoração:'));
+
+const fatoracao = (n) => {
+
+    if ( n<=1){
+        return 1; 
+    } else {        
+        n = n * fatoracao(n - 1);}
+    return n;
+}
+
+fatoracao (num);
+
+console.log (`A Fatoração do número ${num} é ${fatoracao(num)}.`);
